6e0246927518658c008e86349667bbedb965200f
ZEugenii <zinich911@gmail.com>
2023-07-22 19:57:10 +0300

    connect metamask mobile app

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { ethers, formatEther } from "ethers";
+import { ethers, formatEther, getAddress } from "ethers";
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import { Toaster, toast } from "react-hot-toast";
@@ -12,53 +12,75 @@ import { MetaMaskSDK } from "@metamask/sdk";
 export const HomePage = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [currentBalance, setCurrentBalance] = useState("");
-
+  console.log(currentAccount);
   let signer = null;
   let provider = null;
 
-  const connectWallet = async () => {
-    new MetaMaskSDK({
-      useDeeplink: false,
-      communicationLayerPreference: "socket",
-    });
-    if (window.ethereum === null) {
-      toast.error("MetaMask not installed");
-      provider = ethers.getDefaultProvider();
-    } else {
-      try {
-        provider = new ethers.BrowserProvider(window.ethereum);
-        signer = await provider.getSigner();
-        getAccountPath(signer.address);
-        toast.success("Connecting successfully");
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-
-  // const connectWallet = () => {
-  //   const { ethereum } = window;
-  //   if (ethereum && ethereum.isMetaMask) {
-  //     console.log("Ethereum successfully detected!");
-  //     // Access the decentralized web!
+  // const connectWallet = async () => {
+  //   new MetaMaskSDK({
+  //     useDeeplink: true,
+  //     communicationLayerPreference: "socket",
+  //   });
+  //   if (window.ethereum === null) {
+  //     toast.error("MetaMask not installed");
+  //     provider = ethers.getDefaultProvider();
   //   } else {
-  //     console.log("Please install MetaMask!");
+  //     try {
+  //       provider = new ethers.BrowserProvider(window.ethereum);
+  //       signer = await provider.getSigner();
+  //       getAccountPath(signer.address);
+  //       toast.success("Connecting successfully");
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
   //   }
   // };
 
-  const getAccountPath = (accountName) => {
-    setCurrentAccount(accountName);
-    getUserBalance(accountName);
-  };
-
-  const getUserBalance = async (accountAddress) => {
-    try {
-      const balance = await provider.getBalance(accountAddress);
-      setCurrentBalance(Number(formatEther(balance)).toFixed(3));
-    } catch (error) {
-      toast.error(error.message);
+  const connectWallet = async () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      console.log("MetaMask Here!");
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          console.log(result);
+          // setLogged(true);
+          setCurrentAccount(getAddress(result[0]));
+        })
+        .catch((error) => {
+          console.log("Could not detect Account");
+        });
+    } else {
+      console.log("Need to install MetaMask");
+      // onboarding.startOnboarding();
     }
   };
+
+  const handleBalance = (currentAccount) => {
+    window.ethereum
+      .request({ method: "eth_getBalance", params: [currentAccount, "latest"] })
+      .then((balance) => {
+        setCurrentBalance(formatEther(balance));
+      })
+      .catch((error) => {
+        console.log("Could not detect the Balance");
+      });
+  };
+
+  handleBalance(currentAccount);
+
+  // const getAccountPath = (accountName) => {
+  //   setCurrentAccount(accountName);
+  //   getUserBalance(accountName);
+  // };
+
+  // const getUserBalance = async (accountAddress) => {
+  //   try {
+  //     const balance = await provider.getBalance(accountAddress);
+  //     setCurrentBalance(Number(formatEther(balance)).toFixed(3));
+  //   } catch (error) {
+  //     toast.error(error.message);
+  //   }
+  // };
 
   const particlesInit = useCallback(async (engine) => {
     await loadFull(engine);
