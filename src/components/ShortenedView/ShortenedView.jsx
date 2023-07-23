4b701a4651878fc18d1e8d25382974ba8c5af919
ZEugenii <zinich911@gmail.com>
2023-07-23 12:40:16 +0300

    create connection to the metanask mobile app

--- src/components/ShortenedView/ShortenedView.jsx
+++ src/components/ShortenedView/ShortenedView.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export const ShortenedView = ({ address }) => {
+export const ShortenedView = ({ address = "" }) => {
   const shortenedAddress = address.slice(0, 6) + "....." + address.slice(-4);
 
   return (
